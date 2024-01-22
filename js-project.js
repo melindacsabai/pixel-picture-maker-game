bd9b9b3ca97a465a68999ab1e3c3b7c08b2e6da3
Melinda Csabai <melinda.csabai@gmail.com>
2024-01-22 10:48:35 +0100

    changing master to main

---
+++ js-project.js
@@ -0,0 +1,214 @@
+
+var selected = document.querySelector(".yellow");
+selected.onclick = function (event) {
+    selected = "yellow";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".blue");
+selected.onclick = function (event) {
+    selected = "blue";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".pink");
+selected.onclick = function (event) {
+    selected = "pink";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".red");
+selected.onclick = function (event) {
+    selected = "red";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".green");
+selected.onclick = function (event) {
+    selected = "green";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".purple");
+selected.onclick = function (event) {
+    selected = "purple";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+var selected = document.querySelector(".orange");
+selected.onclick = function (event) {
+    selected = "orange";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonLightBlue = document.querySelector(".lightBlue");
+    buttonLightBlue.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+}
+var selected = document.querySelector(".lightBlue");
+selected.onclick = function (event) {
+    selected = "lightBlue";
+    event.target.style.transform = "scale(." + 9 + ")";
+    let buttonBlue = document.querySelector(".blue");
+    buttonBlue.style.transform = "none";
+    let buttonGreen = document.querySelector(".green");
+    buttonGreen.style.transform = "none";
+    let buttonYellow = document.querySelector(".yellow");
+    buttonYellow.style.transform = "none";
+    let buttonPink = document.querySelector(".pink");
+    buttonPink.style.transform = "none";
+    let buttonRed = document.querySelector(".red");
+    buttonRed.style.transform = "none";
+    let buttonPurple = document.querySelector(".purple");
+    buttonPurple.style.transform = "none";
+    let buttonOrange = document.querySelector(".orange");
+    buttonOrange.style.transform = "none";
+}
+
+var reStart = document.querySelector(".reStart");
+reStart.onclick = function (event) {
+    let divek = document.querySelectorAll("div");
+    for (div of divek) {
+
+        div.style.backgroundColor = "white";
+
+
+        let buttonBlue = document.querySelector(".blue");
+        buttonBlue.style.transform = "none";
+        let buttonGreen = document.querySelector(".green");
+        buttonGreen.style.transform = "none";
+        let buttonYellow = document.querySelector(".yellow");
+        buttonYellow.style.transform = "none";
+        let buttonPink = document.querySelector(".pink");
+        buttonPink.style.transform = "none";
+        let buttonRed = document.querySelector(".red");
+        buttonRed.style.transform = "none";
+        let buttonPurple = document.querySelector(".purple");
+        buttonPurple.style.transform = "none";
+        let buttonOrange = document.querySelector(".orange");
+        buttonOrange.style.transform = "none";
+        let buttonLightBlue = document.querySelector(".lightBlue");
+        buttonLightBlue.style.transform = "none";
+
+    }
+}
+
+let divek = document.querySelectorAll("div");
+for (div of divek) {
+    div.onclick = function (event) {
+
+        event.target.style.backgroundColor = selected;
+    }
+    div.ondblclick = function (event) {
+
+        event.target.style.backgroundColor = "white";
+    }
+
+}
+
+let view = document.querySelector(".view");
+view.onclick = function (event) {
+    let divek = document.querySelectorAll("div");
+    for (div of divek) {
+        div.style.border = "none";
+        div.style.transform = ":scale(" + 2 + ")";
+
+    }
+}
+
+let back = document.querySelector(".back");
+back.onclick = function (event) {
+    let divek = document.querySelectorAll("div");
+    for (div of divek) {
+        div.style.border = 1+ "px solid rgb(46, 47, 47)";
+        div.style.transform = "none";
+
+    }
+}
